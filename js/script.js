63648ddf921ad97cd845d0e8879280f001b4ae6c
JHY06 <ouou1326@gmail.com>
2024-10-17 15:37:55 +0900

    메인 페이지 구현중

--- js/script.js
+++ js/script.js
@@ -23,3 +23,16 @@ const artistSwiper = new Swiper(".artist-swiper", {
         el: ".swiper-pagination",
     },
 });
+
+// new production 슬라이드
+const swiper = new Swiper(".newproduction-slide", {
+    slidesPerView: 6, // You can adjust this to show more slides
+    spaceBetween: 10, // Adjust space between slides
+    speed: 1000,
+    autoplay: true,
+    loop: true, // Enable infinite loop
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
